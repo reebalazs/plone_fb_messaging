326418c9bcfd512c9c9f870bc643f5a53af74628
Sushain Cherivirala <sushain@skc.name>
2013-07-26 12:26:13 -0500

    condense into command central
    using routing with templates (fb_activity.html, fb_messaging.html,
    CC.html) to have single JS and CSS (CC.js, CC.css) handle activity
    stream and messaging with index.html as only visibly accessed page

--- plone_fb_messaging/static/CC.js
+++ plone_fb_messaging/static/CC.js
@@ -1,21 +1,138 @@
-var privateChat;
-var privateChatUser;
-var userRef;
-var app = angular.module('messaging', ['firebase']);
-
+var app = angular.module('commandCentral', ['firebase']);
 var firebaseURL = 'https://sushain.firebaseio.com/';
 var onlineRef = new Firebase(firebaseURL + 'presence');
 var connectedRef = new Firebase(firebaseURL + '.info/connected');
-var $el = $('#messagesDiv');
+var userRef;
 
-/* app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/public', {templateUrl: '/index.html', controller: 'MessagingController' })
-        .when('/private/:username', {templateUrl: '/index.html', controller: 'MessagingController' })
-        .otherwise({redirectTo: '/public'});
-}]); */
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/', {templateUrl: 'CC.html', controller: 'CommandCentralController'}).
+      when('/activity', {templateUrl: 'fb_activity.html', controller: 'ActivityStreamController'}).
+      when('/messaging', {templateUrl: 'fb_messaging.html', controller: 'MessagingController'}).
+      otherwise({redirectTo: '/'});
+}]);
 
-app.controller('MessagingController', ['$scope', '$timeout', 'angularFire', 'angularFireCollection', '$q',
+app.controller('CommandCentralController', ['$scope', '$timeout', 'angularFire', 'angularFireCollection', '$q',
     function($scope, $timeout, angularFire, angularFireCollection, $q) {
+        
+    }
+]);
+
+app.controller('ActivityStreamController', ['$scope', '$timeout', 'angularFire', 'angularFireCollection', '$q', '$route',
+    function($scope, $timeout, angularFire, angularFireCollection, $q, $route) {
+        $scope.usernameRegexp = new RegExp('[a-zA-Z0-9.-_]+');
+        var username = $.cookie('username');
+        if(username === undefined || username.search($scope.usernameRegexp) !== 0) {
+            var anonUser = 'Anonymous' + Math.floor(Math.random() * 111);
+            $scope.username = anonUser; //Very bad things happen if two people have the same username
+            $.cookie('username', anonUser);
+        }
+        else if(username.search($scope.usernameRegexp) === 0)
+            $scope.username = username;
+            
+        connectedRef.on('value', function(snap) {
+            if(snap.val() === true) {
+                userRef = onlineRef.child($scope.username);
+                var connRef = userRef.child('online').push(1);
+                userRef.child('lastActive').set(Firebase.ServerValue.TIMESTAMP);
+                userRef.child('online').onDisconnect().remove();
+                userRef.child('logout').onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
+            }
+        });
+    
+        $scope.scroll = function() {
+            $timeout(function() {
+                var $el = $('#activitiesDiv');
+                if($el.length) $el[0].scrollTop = $el[0].scrollHeight;
+            });
+        };
+        
+        $scope.getLastSeen = function() {
+            var deferred = $q.defer();
+            onlineRef.child($scope.username).child('lastSeen').on('value', function(dataSnapshot) {
+                deferred.resolve(dataSnapshot.val());
+                if(!$scope.$$phase) $scope.$apply();  //needed for the resolve to be processed
+            });
+            return deferred.promise;
+        };
+        
+        onlineRef.child($scope.username).child('lastSeen').on('value', function(dataSnapshot) {
+            $scope.lastSeen = dataSnapshot.val();
+        });
+
+        $scope.activities = [];
+        var promise = $scope.getLastSeen();
+        promise.then(function(lastSeen) {
+            $scope.lastSeen = lastSeen;
+            $scope.activities = angularFireCollection(firebaseURL + 'activity', function() {
+                $scope.scroll();
+            });
+        });
+        
+        $scope.markSeen = function() {
+            userRef.child('lastSeen').set(Firebase.ServerValue.TIMESTAMP);
+        };
+        
+        $scope.updateUsername = function() {
+            if($('#username').val() !== '') {
+                var oldUserRef = onlineRef.child($.cookie('username'));
+                var connRef = oldUserRef.child('online').remove();
+                oldUserRef.child('logout').set(Firebase.ServerValue.TIMESTAMP);
+                oldUserRef.child('online').remove();
+                $.cookie('username', $('#username').val());
+                
+                userRef = onlineRef.child($scope.username);
+                connRef = userRef.child('online').push(1);
+            }
+            else
+                $scope.username = $.cookie('username');
+            location.reload(); //username will never change like this in deployment anyways
+        };
+
+        /*This will ensure that if an event expires while displayed on the activity stream page, it will dissapear.
+          However, this will result in recurring JS calls which may be undesirable and a flicker at every iteration.
+          Comment out the setInterval call to disable this functionality */
+        setInterval(refresh, 10000);
+        function refresh() {
+            $scope.activities = angularFireCollection(firebaseURL + 'activity', function() {
+                $scope.scroll();
+                if(!$scope.$$phase) $scope.$apply();
+            });
+        }
+    }
+]);
+
+app.filter('activityFilter', function() {
+    return function(activities, $scope) {
+        var result = [];
+        var lastSeen = $scope.lastSeen === undefined ? -1 : $scope.lastSeen; //-1 will effectively show all activity
+
+        for(var i = 0; i < activities.length; i++) {
+            var activity = activities[i];
+            if(activity.time > lastSeen && activity.expiration > Date.now())
+                result.push(activity);
+        };
+        return result;
+    }
+});
+
+app.filter('millisToReadableDate', function() {
+    return function(date) {
+        return new Date(date).toString();
+    }
+});
+
+app.filter('timeFromNow', function() {
+    return function(date) {
+        return (date - Date.now())/1000 + ' seconds'; //This can be easily improved to increase verbosity
+    }
+});
+
+var privateChat;
+var privateChatUser;
+
+app.controller('MessagingController', ['$scope', '$timeout', 'angularFire', 'angularFireCollection', '$route', '$q',
+    function($scope, $timeout, angularFire, angularFireCollection, $q, $route) {
         $scope.usernameRegexp = new RegExp('[a-zA-Z0-9.-_]+');
         var username = $.cookie('username');
         if(username === undefined || username.search($scope.usernameRegexp) !== 0) {
@@ -68,7 +185,8 @@ app.controller('MessagingController', ['$scope', '$timeout', 'angularFire', 'ang
         $scope.addMessage = function() {
             var userRef = onlineRef.child($scope.username);
             userRef.child('lastActive').set(Firebase.ServerValue.TIMESTAMP);
-            
+            var $el = $('#messagesDiv');
+
             var from = $scope.username;
             var msg = encodeHTML($scope.message);
             if($scope.message.indexOf('/') === 0)
@@ -132,7 +250,7 @@ app.controller('MessagingController', ['$scope', '$timeout', 'angularFire', 'ang
         $scope.scroll = function() {
             $timeout(function() {
                 var $el = $('#messagesDiv');
-                $el[0].scrollTop = $el[0].scrollHeight;
+                if($el.length) $el[0].scrollTop = $el[0].scrollHeight;
             });
         }
     }
@@ -170,6 +288,7 @@ function commandHandler($scope, msg) {
     var command = delim !== -1 ? msg.substring(1, delim) : msg.substr(1);
     var username = $scope.username;
     var usernameRegexp = $scope.usernameRegexp.source;
+    var $el = $('#messagesDiv');
     
     switch(command) {
         case 'msg': {
@@ -328,14 +447,6 @@ app.filter('messageFilter', function() {
     }
 });
 
-function privateWindowURL(sender) {
-    return '//' + location.host + location.pathname + '?user=' + encodeURIComponent(sender);
-}
-
-function scrollWindow($el) {
-    $el.animate({scrollTop: $el[0].scrollHeight}, 500);
-}
-
 app.directive('contenteditable', function() {
     return {
         restrict: 'A',
@@ -373,6 +484,10 @@ app.directive('contenteditable', function() {
         }
     };
 });
+
+function scrollWindow($el) {
+    $el.animate({scrollTop: $el[0].scrollHeight}, 500);
+}
 
 //from http://stackoverflow.com/a/1219983/1266600
 function encodeHTML(value){
