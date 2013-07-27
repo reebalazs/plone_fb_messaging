14d6a87fa386ce0d58502cbfab21679d22fa3714
Balazs Ree <ree@greenfinity.hu>
2013-07-27 19:13:35 +0200

    update, add popup overlay logic

---
+++ plone_fb_messaging/viewlet.py
@@ -0,0 +1,30 @@
+
+from Products.CMFCore.utils import getToolByName
+from plone.app.layout.viewlets import common as base
+
+from .config import get_config
+from .auth import get_auth_token
+
+
+class FirebaseViewlet(base.ViewletBase):
+    """
+    """
+
+    def update(self):
+        super(FirebaseViewlet, self).update()
+        self.auth_token = get_auth_token(self.context, self.request)
+        self.config = get_config()
+        # Is the product installed?
+        # We need to check it, as this information is not obvious based on
+        # the configuration alone.
+        # The importance of this check is to avoid the viewlet being
+        # rendered before the product is installed with quickinstaller
+        # in a newly created portal.
+        qi = getToolByName(self.context, 'portal_quickinstaller')
+        self.is_installed = qi.isProductInstalled('plone_fb_messaging')
+
+    def render(self):
+        if self.is_installed and self.auth_token:
+            return super(FirebaseViewlet, self).render()
+        else:
+            return ''
