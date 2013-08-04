c48fcbe3b9d400187e264b3a0ee3eb2ed42e2150
Balazs Ree <ree@greenfinity.hu>
2013-08-04 17:33:16 +0200

    Autoscroll with animation

--- plone_fb_messaging/static/CC.js
+++ plone_fb_messaging/static/CC.js
@@ -262,8 +262,10 @@ app.directive('autoScroll', function ($timeout) {
     return function (scope, elements, attrs) {
         scope.$watch("messages.length", function() {
             $timeout(function() {
-                elements[0].scrollTop = elements[0].scrollHeight;
-                //$el.animate({scrollTop: $el[0].scrollHeight}, 500);
+                //elements[0].scrollTop = elements[0].scrollHeight;
+                elements.animate({
+                    scrollTop: elements[0].scrollHeight
+                }, 500);
             });
         });
     };
